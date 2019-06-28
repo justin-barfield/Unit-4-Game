2171857d8331c9a467ed15e043c6b23129658892
barfiejus <justin.kyle.barfield@gmail.com>
2019-06-27 21:21:08 -0400

    jquery and javascript added to generate random numbers for the number to guess and the buttons. Button values are calculated by dividing by the original number. Functions to run though the program hav been set. Resetting the game works, but wins and losses will not update within the function.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+/* Structure Notes */
+
+// Display buttons 'crystals'
+// Generate random number and display on screen
+    // Set score to 0 (function)
+    // Set hidden value to buttons
+        // Math to get values for buttons that allow adding/multiplying to random number?
+    // Buttons add to score
+// Win if matching random number
+    // Add to Wins
+// Lose if over random number
+    // Add to loses
+// Restart game
+    // Get new random number
+    // Get new button values
+    // Reset score counter
+
+function randomNumber() {
+    Math.floor(Math.random * 100);
+};
+
